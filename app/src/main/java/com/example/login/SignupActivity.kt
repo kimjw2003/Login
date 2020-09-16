47c0fa6407fae30076d2996610c6bd5b7295dbfa
kimjw2003 <kimjw111003@gmail.com>
2020-09-16 16:30:11 +0900

    커밋

--- app/src/main/java/com/example/login/SignupActivity.kt
+++ app/src/main/java/com/example/login/SignupActivity.kt
@@ -1,11 +1,25 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        signup_Btn.setOnClickListener {
+            var intent = Intent(this, SignupActivity::class.java)
+
+            if(signUp_pw == signUp_repw){
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
